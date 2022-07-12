dc3ff1f9bbf27e861af9c3a4869d06231b4cea9d
hlomla <hlomla.tapuko@gmail.com>
2022-07-12 14:38:44 +0200

    setting up the database

--- server/sql/tables.sql
+++ server/sql/tables.sql
@@ -1,15 +1,36 @@
+create table passengers(
+    id serial not null primary key,
+    name text not null,
+    surname text not null,
+    username text not null,
+    password text not null
+);
+
+create table taxi_driver(
+    id serial not null primary key,
+    name text not null,
+    surname text not null,
+    username text not null,
+    password text not null,
+    reg_number varchar not null,
+    taxi_code varchar not null,
+    foreign key(taxi_code) references taxi_owners(unique_code)
+);
+
 create table taxi_owners(
     id serial not null primary key,
-    name varchar,
-    surname int,
-    unique_code text,
-    his_taxis text,
-    drivers_name text,
+    name text not null,
+    surname text not null,
+    username text not null,
+    unique_code varchar not null,
+    his_taxis varchar not null,
+    drivers_name text not null,
+    password text not null
 );
 
 create table taxi_trips(
     id serial not null primary key,
-    reg_number varchar,
+    reg_number varchar not null,
     route int,
-    drivers_name text,
+    drivers_name text not null
 );
