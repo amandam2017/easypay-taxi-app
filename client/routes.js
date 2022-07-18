96f80411c1d5037790ddfb9c8bad123cd305c5a2
hlomla <hlomla.tapuko@gmail.com>
2022-07-18 12:39:41 +0200

    can display taxis

--- client/routes.js
+++ client/routes.js
@@ -4,6 +4,7 @@ const remote_url = 'https://easypay-taxi-app.herokuapp.com'
 
 const Routes = ()=>{
     return{
+        taxis: [],
         routes:{
             user_departure:'',
             user_destination:''
@@ -11,13 +12,14 @@ const Routes = ()=>{
         
         findTaxiByRoute(){
             axios
-            .post(`${remote_url}/api/taxis`, this.routes)
+            .post(`${localUrl}/api/taxis`, this.routes)
             // alert(`${this.depature}, ${this.destination}`)
             // .then(r => r.json())
             .then(results=>{
                 // console.log(data);
                 console.log(results.data);
-                this.routes = results.data
+                // this.routes = results.data
+                this.taxis = results.data.data
                 // this.destination = results.data
             })
             .catch(err => console.log(err))
