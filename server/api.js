879b09cfe3e0105269ffbfc0d814a6020c69abf4
amandam2017 <amandamaarman19911@gmail.com>
2022-08-18 17:26:46 +0200

    added changes

--- server/api.js
+++ server/api.js
@@ -23,7 +23,6 @@ const api = (app, db) => {
             const salt = await bcrypt.genSalt(saltRounds);
             const hash = await bcrypt.hash(password, salt)
             const user = await db.oneOrNone('select * from users where username = $1', [username])
-            // await db.oneOrNone(`update users set role='driver' where username = $1`, [username])
             if (user == null) {
                 await db.none('insert into users (name, surname, username, password, role) values ($1, $2, $3, $4, $5)', [name, surname, username, hash, role]);
                 res.json({
